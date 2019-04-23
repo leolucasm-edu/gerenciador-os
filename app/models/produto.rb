cebfb9263b0d5080129703065a94aa05eb48eadf
Leonardo Melo <leolucasm@edu.univali.br>
2019-04-22 23:30:27 -0300

    Melhorando views (2)

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,2 +1,3 @@
 class Produto < ApplicationRecord
+  validates :descricao, presence: true
 end
--- app/models/servico.rb
+++ app/models/servico.rb
@@ -1,2 +1,4 @@
 class Servico < ApplicationRecord
+  validates :descricao, presence: true
+  validates :preco_hora, :numericality => { :greater_than => 0, message: "deve ser maior que R$0,00"}
 end
