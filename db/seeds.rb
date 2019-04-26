89c5e3cbf9e70dc911c57f9177bd3f5b20b88b61
Leonardo Melo <leonardo@ibfsistemas.com.br>
2019-04-26 13:26:01 -0300

    Criando seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Gerando os produtos..."
+  Produto.create!([{descricao: "Pneu", preco_compra: 100, preco_venda: 160},
+               {descricao: "Farol", preco_compra: 50, preco_venda: 80},
+               {descricao: "Freio", preco_compra: 35, preco_venda: 65}])
+puts "Gerando os produtos... [OK]"
+
+puts "Gerando os servicos..."
+  Servico.create!([{descricao: "Pintura", preco_hora: 300},
+               {descricao: "Troca de óleo (carro)", preco_hora: 100},
+               {descricao: "Troca de óleo (moto)", preco_hora: 20}])
+puts "Gerando os servicos... [OK]"
